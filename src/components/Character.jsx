8076cd6ff4d98fadc71494438becca2929fae54a
Ro1984Bi <bilbaorodrigojavi@outlook.com>
2022-11-04 13:04:50 -0300

    react project

---
+++ src/components/Character.jsx
@@ -0,0 +1,13 @@
+
+
+function Character({character}) {
+  return (
+    <div key={character.id} className="text-center p-5">
+    <h3>{character.name}</h3>
+    <img src={character.image} alt={character.name} className="img-fluid rounded-pill"/>
+    <p>{character.origin.name}</p>
+  </div>
+  )
+}
+
+export default Character
